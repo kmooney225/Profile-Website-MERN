64986b97a904b877aa578d7f111e3388c4d16f23
kmooney225 <kmooney225@gmail.com>
2022-09-08 17:37:46 -0600

    setting up contact page

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,19 +1,67 @@
 import React from "react";
 
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation"
+import { pageAnimation, titleAnim } from "../animation";
+import styled from "styled-components";
 
 const ContactMe = () => {
     return(
-        <motion.div
+        <ContactStyle
         exit="exit"
         variants={pageAnimation} 
         initial="hidden"
         animate="show"
         style={{background: "#fff"}}>
-            <h1>Our Work</h1>
-        </motion.div>
+            <Title>
+                <motion.h2 variants={{titleAnim}}> 
+                    Get in Touch!
+                </motion.h2>
+            </Title>
+        <div>
+            <Social variants={titleAnim}>
+                <Circle>
+                    <h2>Send An Email</h2>
+                </Circle>
+            </Social>
+            <Social variants={titleAnim}>
+                <Circle>
+                    <h2>GitHub</h2>
+                </Circle>
+            </Social>
+            <Social variants={titleAnim}>
+                <Circle>
+                    <h2>Linkedin</h2>
+                </Circle>
+            </Social>
+        </div>
+        </ContactStyle>
     )
 }
+
+const ContactStyle = styled(motion.div)`
+    padding: 5rem 10rem;
+    color: #150000;
+    min-height: 90vh;
+`
+const Title = styled.div`
+    margin-bottom: 4rem;
+    color: black;
+`
+
+
+const Circle = styled.div`
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    background: #150000;
+`
+
+const Social = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    h2{
+        margin: 2rem;
+    }
+`
 
 export default ContactMe;
