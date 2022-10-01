ba7ba934ca0ac8e2c8cd970c9ab5233fcfadc379
kmooney225 <kmooney225@gmail.com>
2022-10-01 15:05:39 -0600

    data for timeline elements in projects section

---
+++ src/components/ProjectsTimelineElements.js
@@ -0,0 +1,27 @@
+
+const ProjectsTimelineElements = [
+    {
+        id: 1,
+        title: "Music Player",
+        description:
+          "A modern music player created using React.js.",
+        buttonText: "View Project",
+        icon: "work",
+      },
+      {
+        id: 2,
+        title: "3D Tetromino",
+        description:
+          "A C# game recreating a 3d version of Tetris. Made in Unity.",
+        buttonText: "View Project",
+        icon: "work",
+      },
+      {
+        id: 3,
+        title: "X-plore: The Country Stats App",
+        description:
+          "A React App showing statistics of all countries.",
+        buttonText: "View Project",
+        icon: "work",
+      },
+    ]
