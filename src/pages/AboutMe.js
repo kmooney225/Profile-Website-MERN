01cf6df4875b8dab6765411e58faa5196a3524ef
kmooney225 <kmooney225@gmail.com>
2022-10-22 09:41:14 -0600

    removing unneeded modules

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,7 +1,6 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
-import StackSection from "../components/StackSection";
 import { motion } from "framer-motion";
 import { pageAnimation } from "../animation"
 import ScrollTop from "../components/ScrollTop";
