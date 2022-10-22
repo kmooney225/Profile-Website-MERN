4718926c9a42a2ece78b14076acfe5f20fced7b7
kmooney225 <kmooney225@gmail.com>
2022-10-22 12:20:52 -0600

    updating youtube links

--- src/components/MusicTimelineElements.js
+++ src/components/MusicTimelineElements.js
@@ -5,17 +5,20 @@ const MusicTimelineElements = [
         id: 1,
         title: "I'm Not The Only One",
         video: "https://www.youtube.com/embed/ukYNf113pMg?start=140",
+        link: "https://www.youtube.com/watch?v=ukYNf113pMg",
       },
 
       {
         id: 2,
         title: "Thinking Out loud",
         video: "https://www.youtube.com/embed/VXkUa66s_WA",
+        link: "https://www.youtube.com/watch?v=VXkUa66s_WA",
       },
       {
         id: 3,
         title: "Just The 2 Of Us",
         video: "https://www.youtube.com/embed/QateISHryec",
+        link: "https://www.youtube.com/watch?v=QateISHryec"
       },
     ]
 
